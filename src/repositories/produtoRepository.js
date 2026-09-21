ebbc1baf7080be1fd39281ed5ac34a3e7974e49d
Matson Lima <matson_lima@outlook.com>
2026-09-21 11:51:11 -0300

    Adicionado pastas e arquivos

--- src/repositories/produtoRepository.js
+++ src/repositories/produtoRepository.js
@@ -2,14 +2,20 @@ const pool = require('../config/db');
 
 
 const getAllProdutos = async () => {
+    console.log("Cheguei no repository");
     const sql = 'SELECT * FROM produtos';
     const resultado = await pool.query(sql);
 
     return resultado.rows;
 };
 
-const getProdutoByID = async () => {
-    
+const getProdutoByID = async (id) => {
+    const valor = [id];
+    const sql = 'SELECT * FROM produtos WHERE id = $1';
+    const resultado = await pool.query(sql, valor);
+
+    return resultado.rows[0];
 };
+
 
 module.exports = {getAllProdutos, getProdutoByID};
